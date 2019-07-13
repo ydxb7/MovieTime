ae97d8bb0046a70df39dae2fcecd7869dc0701c3
Yini <ydxb7@mail.missouri.edu>
2019-07-12 20:45:26 -0700

    cannot keep the state, when navigate back to ViewPagerFragment

--- app/src/main/java/ai/tomorrow/movietime/viewPages/ViewPatersFragment.kt
+++ app/src/main/java/ai/tomorrow/movietime/viewPages/ViewPatersFragment.kt
@@ -1,5 +1,6 @@
 package ai.tomorrow.movietime.viewPages
 
+import ai.tomorrow.movietime.R
 import ai.tomorrow.movietime.databinding.FragmentViewPagersBinding
 import ai.tomorrow.movietime.viewPages.SimpleFragmentPagerAdapter
 import android.os.Bundle
@@ -7,12 +8,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
 
 class ViewPatersFragment: Fragment() {
+//    lateinit var binding: FragmentViewPagersBinding
+    lateinit var viewPager: ViewPager
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         val binding = FragmentViewPagersBinding.inflate(inflater)
-        val viewPager = binding.viewpager
+        viewPager = binding.viewpager
 
         // Create an adapter that knows which fragment should be shown on each page
         val adapter = SimpleFragmentPagerAdapter(childFragmentManager)
@@ -22,11 +28,27 @@ class ViewPatersFragment: Fragment() {
 
         // Give the TabLayout the ViewPager
         val tabLayout = binding.slidingTabs
-        tabLayout.setupWithViewPager(viewPager)
+//        tabLayout.setupWithViewPager(viewPager)
 
+
+        viewPager.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tabLayout))
+        tabLayout.setOnTabSelectedListener(onTabSelectedListener)
 
 
         return binding.root
     }
 
+    private val onTabSelectedListener = object : TabLayout.OnTabSelectedListener {
+        override fun onTabSelected(tab: TabLayout.Tab) {
+            viewPager.setCurrentItem(tab.getPosition())
+        }
+
+        override fun onTabUnselected(tab: TabLayout.Tab) {
+
+        }
+
+        override fun onTabReselected(tab: TabLayout.Tab) {
+
+        }
+    }
 }
